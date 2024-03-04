52b285aa97ae19a14a2961449e90f349159e0b62
MarcoLuw <hoanganhnk2002@gmail.com>
2024-03-05 00:49:22 +0700

    Untrack files and update .gitignore

--- helper/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-echo "Starting docker-compose..."
-
-docker-compose -f ./backend/docker-compose.yml up
-docker-compose -f ./frontend/docker-compose.yml up
--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-echo "Starting docker-compose..."
-
-docker-compose -f ./backend/docker-compose.yml up
