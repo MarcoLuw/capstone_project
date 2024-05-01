d5fdbf27e18e666b9b8fa0c8894a32321379478c
kayden ho <kayden.ho.int@shopback.com>
2024-05-01 17:40:15 +0700

    test spark cluster done

--- data_service/download-jarfiles.sh
+++ data_service/download-jarfiles.sh
@@ -4,5 +4,5 @@ wget --no-verbose https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-c
 wget --no-verbose https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk/1.12.541/aws-java-sdk-1.12.541.jar
 wget --no-verbose https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.12.541/aws-java-sdk-bundle-1.12.541.jar
 wget --no-verbose https://repo1.maven.org/maven2/io/delta/delta-core_2.13/2.4.0/delta-core_2.13-2.4.0.jar
-wget --no-verbose https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.6/hadoop-aws-3.3.6.jar
+wget --no-verbose https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.4/hadoop-aws-3.3.4.jar
 wget --no-verbose https://repo1.maven.org/maven2/org/postgresql/postgresql/42.7.2/postgresql-42.7.2.jar
