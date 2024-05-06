e22ceff89073ad4ae54fd6a0731d375979739c39
kayden ho <kayden.ho.int@shopback.com>
2024-05-06 21:34:37 +0700

    update hive config

--- data_service/download-jarfiles.sh
+++ data_service/download-jarfiles.sh
@@ -4,6 +4,6 @@ echo ${PWD}
 wget --no-verbose https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/3.2.0/mariadb-java-client-3.2.0.jar
 wget --no-verbose https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk/1.12.541/aws-java-sdk-1.12.541.jar
 wget --no-verbose https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.12.541/aws-java-sdk-bundle-1.12.541.jar
-wget --no-verbose https://repo1.maven.org/maven2/io/delta/delta-core_2.13/2.4.0/delta-core_2.13-2.4.0.jar
+wget --no-verbose https://repo1.maven.org/maven2/io/delta/delta-core_2.12/2.4.0/delta-core_2.12-2.4.0.jar
 wget --no-verbose https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.4/hadoop-aws-3.3.4.jar
 wget --no-verbose https://repo1.maven.org/maven2/org/postgresql/postgresql/42.7.2/postgresql-42.7.2.jar
